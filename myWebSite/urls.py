b90b6a9f9a692c95d9ac24e4d4b4240c43f24c9b
0ussamazed <oussamazaddam.2003@gmail.com>
2025-11-05 00:06:24 +0100

    تحديث إعدادات static و media للنشر على Render

--- myWebSite/urls.py
+++ myWebSite/urls.py
@@ -1,25 +1,100 @@
-"""myWebSite URL Configuration
+from pathlib import Path
+import os
 
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-from django.contrib import admin
-from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
+BASE_DIR = Path(__file__).resolve().parent.parent
 
-urlpatterns = [
-    path('admin/', admin.site.urls, name="admin"),
-    path('',include('lms_app.urls')),
+# -------------------------
+# أسرار المشروع
+# -------------------------
+SECRET_KEY = os.environ.get('SECRET_KEY')
+DEBUG = os.environ.get('DEBUG', 'False') == 'True'
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOSTS', '').split(',')
+
+# -------------------------
+# التطبيقات
+# -------------------------
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'lms_app',
 ]
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+ROOT_URLCONF = 'myWebSite.urls'
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+WSGI_APPLICATION = 'myWebSite.wsgi.application'
+
+# -------------------------
+# قاعدة البيانات
+# -------------------------
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
+
+# -------------------------
+# Static files
+# -------------------------
+STATIC_URL = '/static/'
+
+# هذا المجلد سيجمع فيه كل ملفات static بعد collectstatic
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# هنا المجلدات التي تحتوي ملفات static الأصلية (قبل جمعها)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'myWebSite/static')
+]
+
+# -------------------------
+# Media files
+# -------------------------
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+# -------------------------
+# نصائح للنشر على Render
+# -------------------------
+# 1. قبل رفع المشروع: 
+#    python manage.py collectstatic
+# 2. ارفع المجلدات الناتجة:
+#    - staticfiles/
+#    - media/
+# 3. في HTML:
+#    - استخدم {% load static %} و {% static '...' %}
+#    - استخدم {{ book.image.url }} للصور من media
+# 4. إذا تريد رفع ملفات كبيرة على السحابة استخدم:
+#    - AWS S3 أو Cloudinary للـMEDIA وSTATIC
+# -------------------------
+
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
